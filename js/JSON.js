cd3d6bef7f9d1428e8c8ecf224002fb190eab883
b79789 <bstacks78@fullsail.edu>
2013-07-08 08:23:19 -0400

    Added my VFW project to my new repositories

---
+++ js/JSON.js
@@ -0,0 +1,25 @@
+var json = {
+	"name1": {
+		"places": ["People","2"],
+		"fName": ["Name","Brian"],
+		"eMail": ["Email" ,"test@me.com"],
+		"pWord": ["Password","test"],
+		"eDate": ["Date","11/12/2013"],
+		"place": ["Place","applebee, no chili"],
+		"eTime": ["Time","1:oopm"],
+		"comments": ["Comments","Testing"]
+	},
+	"name2": {
+		"places": ["People","3"],
+		"fName": ["Name","jon"],
+		"eMail": ["Email" ,"test@you.com"],
+		"pWord": ["Password","test2"],
+		"eDate": ["Date","10/11/2014"],
+		"place": ["Place","applebee, chili"],
+		"eTime": ["Time","2:ooam"],
+		"comments": ["Comments","Testing2"]
+	}
+
+	
+	
+}
